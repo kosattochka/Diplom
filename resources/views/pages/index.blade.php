252072b28e83d0e5a2d78d17a347db1e18f79af4
Kosattochka <125015396+kosattochka@users.noreply.github.com>
2025-03-03 19:42:08 +0500

    новости

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -86,6 +86,18 @@
                     <span>Оценка в 2gis</span>
                 </a>
             </div>
+            <div class="New-block">
+                <h1>Новости</h1>
+                <img src="/img/new-foto.svg" alt="">
+                <div class="row">
+                    <figure></figure>
+                    <span>27.12.24</span>
+                </div>
+                <h1>Идеальный зимний уикенд</h1>
+                <span>Зима — время для снежных пейзажей, активного отдыха и уютных вечеров у камина. Как совместить спорт, отдых и комфорт?</span>
+                <button>Подробнее</button>
+            </div>
+
         </aside>
     </div>
 @endsection
