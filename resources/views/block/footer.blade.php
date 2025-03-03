82f110c282b204b2460860b40d292c97549d0afb
Kosattochka <125015396+kosattochka@users.noreply.github.com>
2025-03-04 00:16:08 +0500

    Update footer.blade.php

--- resources/views/block/footer.blade.php
+++ resources/views/block/footer.blade.php
@@ -4,26 +4,26 @@
             <div class="column">
                 <div class="pank">
                     <img src="/img/footer-icon-phone.svg" alt="">
-                    <a href="">Офис продаж (звонок бесплатный) 8 (800) 600-93-44</a>
+                    <a href="">Офис продаж (звонок бесплатный) {{$phone}}</a>
                 </div>
                 <div>
                     <img src="/img/footer-icon-telegram.svg" alt="">
-                    <a href="https://t.me/Pavlovskij_Park">Телеграм</a>
+                    <a href="{{$telegram}}">Телеграм</a>
                 </div>
             </div>
             <div class="column">
                 <div>
                     <img src="/img/footer-icon-vk.svg" alt="">
-                    <a href="https://vk.com/club23119361">Вконтакте</a>
+                    <a href="{{$vk}}">Вконтакте</a>
                 </div>
                 <div class="pank">
                     <img src="/img/footer-icon-email.svg" alt="">
-                    <a href="">E-mail: pavlovpark@yandex.ru</a>
+                    <a href="">E-mail: {{$email}}</a>
                 </div>
             </div>
         </div>
         <div class="footer-info">
-            <p>Адрес офиса продаж: г. Уфа, ул. Российская, 98/2</p>
+            <p>Адрес офиса продаж: {{$address}}</p>
             <p class="pank">На нашем сайте есть <a href="">политика конфиденциальности.</a></p>
             <p class="pank">© 2009-2025 Все права защищены Круглогодичный центр отдыха "Павловский парк". Официальный сайт</p>
         </div>
