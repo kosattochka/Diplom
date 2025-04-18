3c692160bdb12a4b7da37f0a30efef86488aa9b8
Roltan <KhalikoR@yandex.ru>
2025-04-18 16:58:27 +0500

    слайдер в деталке комнаты

--- resources/views/pages/detail-room.blade.php
+++ resources/views/pages/detail-room.blade.php
@@ -43,6 +43,16 @@
       </div>
     </div>
     <div class="six"><p><span><img src="/img/rooms/icon-title-room.svg" alt="">Другие номера</span></p></div>
+    @php
+    $slider = array_fill(0, 4, view('element.card.card', ['name'=>'123тест', 'text'=>'fjfkf', 'img'=>'/img/albums/winter-album2.png', 'link'=> '/'])->render())
+    @endphp
+
+    @include('block.slider', [
+        'id'=>2,
+        'desktopCount' => 2,
+        'mobileCount' => 1,
+        'elements' => $slider
+    ])
 
     @include('block.footer', [
         'phone' =>$contacts->phone,
