d5036be1a1ac2ac54651e5489a43a72b6a52afd2
Roltan <KhalikoR@yandex.ru>
2025-04-24 14:52:16 +0500

    сидер дочерних услуг

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -13,7 +13,12 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i=0;$i<10;$i++)
+        for ($i = 0; $i < 10; $i++)
             Service::factory()->create();
+
+        $id = Service::inRandomOrder()->first()->id;
+        for ($i = 0; $i < 5; $i++) {
+            Service::factory()->create(['parent_id' => $id]);
+        }
     }
 }
